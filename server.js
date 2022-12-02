09555e0735893a426cb7b8758dacae22685617df
AntonZakhlebayeu <antonzakhlebayeu@gmail.com>
2022-12-02 16:56:09 +0300

    Fixes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import { hostname } from 'os';
-import { createServer } from 'http';
+const { hostname } = require('os');
+const http = require('http');
 const message = `Hello World from ${hostname()}\n`; 
 const port = 8080; 
-const server = createServer((req, res) => { 
+const server = http.createServer((req, res) => { 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(message);
